1842290ec87c9de6bdfcf32dd8a41c9b6d7c03c1
nuhaydogdu <nuhaydd25@gmail.com>
2023-12-20 23:30:15 +0300

    created api project

---
+++ API/InfoGraphX-API/InfoGraphX-API/Models/ForeignTradeValueIndice.cs
@@ -0,0 +1,11 @@
+﻿namespace InfoGraphX_API.Models
+{
+    public class ForeignTradeValueIndice
+    {
+        public int Id { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public float ExportUniteValue { get; set; }
+        public float ImportUniteValue { get; set; }
+    }
+}
---
+++ API/InfoGraphX-API/InfoGraphX-API/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace InfoGraphX_API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int Age { get; set; }
+    }
+}
