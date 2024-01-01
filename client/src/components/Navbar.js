7106d24785dba1d294d62005d52ea085fee0e2a3
alimert1 <yilmazalimert20@gmail.com>
2024-01-02 01:20:45 +0300

    added logo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../App.css'
 import {Container, Row, Col, Image} from "react-bootstrap";
-
+import logo from '../images/logo.png'
 function Navbar() {
   return (
     <Container className='custom-container mb-3 p-3'>
@@ -10,10 +10,12 @@ function Navbar() {
                 <Image src='https://cdn.freelogovectors.net/wp-content/uploads/2020/07/manisa-celal-bayar-universitesi_logo.png' style={{ width: '200px', height: '200px' }} rounded/>
             </Col>
             <Col className='d-flex align-items-center justify-content-center'>
-            <h2>SOFTWARE PROJECT</h2>
+            
+           <h1>Software Project</h1>
+            
             </Col>
             <Col className='pe-5 d-flex align-items-end justify-content-end'>
-                <Image src='https://cdn.freelogovectors.net/wp-content/uploads/2020/07/manisa-celal-bayar-universitesi_logo.png' style={{ width: '200px', height: '200px' }} rounded/>
+                <Image src= {logo} style={{ width: '200px', height: '200px' }} rounded/>
             </Col>
         </Row>
     </Container>
