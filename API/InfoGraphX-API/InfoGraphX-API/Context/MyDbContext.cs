04f4534c79ca151fae9595e30261afa1b635096c
nuhaydogdu <nuhaydd25@gmail.com>
2024-01-02 17:45:50 +0300

    TestCases

--- API/InfoGraphX-API/InfoGraphX-API/Context/MyDbContext.cs
+++ API/InfoGraphX-API/InfoGraphX-API/Context/MyDbContext.cs
@@ -20,8 +20,5 @@ namespace InfoGraphX_API.Context
         public DbSet<HappinessLevelByAgeGroup> HappinessLevelByAgeGroups { get; set; }
         public DbSet<HappinesRates> HappinesRates{ get; set; }
 
-        //public DbSet<Tufe> Tufe { get; set; }
-
-
     }
 }
